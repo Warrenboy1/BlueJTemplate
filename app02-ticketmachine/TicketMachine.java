8a26f3339950170f8ccce9b9798287a93ef9e589
Warrenboy1 <72131977+Warrenboy1@users.noreply.github.com>
2020-10-08 22:50:33 +0100

    added separate tickets rather same location is printed

--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -22,8 +22,13 @@ public class TicketMachine extends Ticket
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    
+    
+   
+    
+    
 
-    /**
+     /**
      * Create a machine that issues tickets of the given price.
      */
     public TicketMachine()
@@ -34,7 +39,7 @@ public class TicketMachine extends Ticket
         
     }
 
-    /**
+     /**
      * Return The amount of money already inserted for the
      * next ticket.
      */
@@ -142,9 +147,9 @@ public class TicketMachine extends Ticket
      * reduce the current balance by the ticket price. Print
      * an error message if more money is required.
      */
-    public void printTicket()
+    public void printTicketHighWycombe()
     {
-        if(balance >= price) 
+        if(balance >= HighWycombe) 
         {
             // Simulate the printing of a ticket.
             System.out.println("########NO########");
